00178f3e1583a38f13ee823454537f4a1c4c64b9
Flockyy <florian.abgrall750@gmail.com>
2023-03-01 11:31:52 +0100

    pred route and model

---
+++ app/server/models/prediction.py
@@ -0,0 +1,33 @@
+from typing import Optional
+
+from pydantic import BaseModel, Field
+
+class PredictionSchema(BaseModel):
+    text: str = Field(...)
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "text": "Hello there",
+            }
+        }
+
+class UpdatePredictionModel(BaseModel):
+    text: Optional[str]
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "text": "Hello there",
+            }
+        }
+
+def ResponseModel(data, message):
+    return {
+        "data": [data],
+        "code": 200,
+        "message": message,
+    }
+
+def ErrorResponseModel(error, code, message):
+    return {"error": error, "code": code, "message": message}
